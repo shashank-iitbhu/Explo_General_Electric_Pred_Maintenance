5bf86623dec9998e52522fb6be1047a0e0320188
Shashank Mittal <shashank.mittal.mec22@itbhu.ac.in>
2024-05-09 23:40:49 +0530

    value fix
    Signed-off-by: Shashank Mittal <shashank.mittal.mec22@itbhu.ac.in>

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ def predict_failure(model, type_of_material, air_temperature, process_temperatur
 # Sidebar inputs
 st.sidebar.title("Enter Machine Parameters")
 type_of_material = st.sidebar.slider("Type of Material", 1, 3, 2)
-air_temperature = st.sidebar.slider("Air Temperature (Kelvin)", 290, 310, 300)
-process_temperature = st.sidebar.slider("Process Temperature (Kelvin)", 290, 310, 300)
-rotational_speed = st.sidebar.slider("Rotational Speed", 0, 100, 50)
-torque = st.sidebar.slider("Torque", 0, 100, 50)
-tool_wear = st.sidebar.slider("Tool Wear", 0, 100, 50)
+air_temperature = st.sidebar.slider("Air Temperature (Kelvin)", 290, 320, 305)
+process_temperature = st.sidebar.slider("Process Temperature (Kelvin)", 290, 330, 310)
+rotational_speed = st.sidebar.slider("Rotational Speed", 1200, 3000, 2100)
+torque = st.sidebar.slider("Torque", 0, 80, 40)
+tool_wear = st.sidebar.slider("Tool Wear", 0, 300, 150)
 
 # Prediction
 logistic_regression_prediction = predict_failure(logistic_regression_model, type_of_material, air_temperature, process_temperature, rotational_speed, torque, tool_wear)
